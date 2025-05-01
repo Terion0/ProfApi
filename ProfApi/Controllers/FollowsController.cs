8e40d2d912d5dc52efdb8ddd746768357bf07c9d
Chema <josemgarciasanchz@gmail.com>
2025-05-01 12:50:13 +0200

    v 1.2

--- ProfApi/Controllers/FollowsController.cs
+++ ProfApi/Controllers/FollowsController.cs
@@ -19,7 +19,7 @@ namespace ProfApi.Controllers
         public FollowsController(ProfDbContext context, ILogger<FollowsController> logger)
         {
             _context = context;
-            _logger = logger;
+            _logger = logger;ass
         }
 
         [HttpGet("followers/{userId}")]
